41cd9198851abd0db8a035348197806b8ef6cda4
Mohammed Ashour <mashour8@gmail.com>
2024-07-27 01:18:04 +0200

    first commit

---
+++ src/routes/login/login.jsx
@@ -0,0 +1,53 @@
+import "./login.scss";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
+
+function Login() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
+      localStorage.setItem("user", JSON.stringify(res.data));
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data.message);
+      // console.log("Error :", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <form onSubmit={handelSubmit}>
+          <h1>Welcome back</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="password" type="password" placeholder="Password" />
+          <button disabled={isLoading}>Login</button>
+          {error && <span> {error}</span>}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
